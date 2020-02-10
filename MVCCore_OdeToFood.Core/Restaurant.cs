99239c2a2239b54828cf903d9df501e92bc37203
KatarinaSundqvist <singsongkat@live.com>
2020-02-10 11:40:05 +0100

    Added a form for editing a restaurant, and added validation for the edits

--- MVCCore_OdeToFood.Core/Restaurant.cs
+++ MVCCore_OdeToFood.Core/Restaurant.cs
@@ -1,8 +1,13 @@
-﻿namespace MVCCore_OdeToFood.Core {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCCore_OdeToFood.Core {
 
     public class Restaurant {
         public int Id { get; set; }
+        [Required]
+        [StringLength(80)]
         public string Name { get; set; }
+        [Required, StringLength(255)]
         public string Location { get; set; }
         public CuisineType Cuisine { get; set; }
     }
--- MVCCore_OdeToFood.Data/IRestaurantData.cs
+++ MVCCore_OdeToFood.Data/IRestaurantData.cs
@@ -6,6 +6,8 @@ namespace MVCCore_OdeToFood.Data {
     public interface IRestaurantData {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        int Commit();
     }
 
     public class InMemoryRestaurantData : IRestaurantData {
@@ -21,6 +23,20 @@ namespace MVCCore_OdeToFood.Data {
 
         public Restaurant GetById(int id) {
             return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant) {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null) {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+
+        public int Commit() {
+            return 0;
         }
 
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null) {
