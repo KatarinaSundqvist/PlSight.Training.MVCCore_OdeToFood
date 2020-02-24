378b0ae0b84add7380af334dc1b52ae9a3219b83
KatarinaSundqvist <singsongkat@live.com>
2020-02-24 10:11:52 +0100

    Added custom logging and looked at configuration

--- MVCCore_OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ MVCCore_OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using MVCCore_OdeToFood.Core;
 using MVCCore_OdeToFood.Data;
 using System.Collections.Generic;
@@ -10,17 +11,20 @@ namespace MVCCore_OdeToFood.Pages.Restaurants {
 
         private readonly IConfiguration config;
         private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
 
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
-        public ListModel(IConfiguration config, IRestaurantData restaurantData) {
+        public ListModel(IConfiguration config, IRestaurantData restaurantData, ILogger<ListModel> logger) {
             this.config = config;
             this.restaurantData = restaurantData;
+            this.logger = logger;
         }
         public void OnGet() {
+            logger.LogError("Executing ListModel");
             Message = config["Message"];
             Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
         }
